58496fb65dbd138a52b4a7479978c10a4aacf5fd
vishwajitvm <vishwajitmall50@gmail.com>
2023-02-01 22:22:29 +0530

    lorem ipsum

--- src/components/PassFunctionAsProps.js
+++ src/components/PassFunctionAsProps.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function PassFunctionAsProps() {
   return (
     <div>
-      hello, world
+      <p>Hello world</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem, provident ut doloremque, enim cumque repellat optio laudantium harum hic ad. Quibusdam ducimus assumenda adipisci delectus, nemo culpa, consequuntur quae ex magnam iure architecto at mollitia veritatis cumque dolore odio impedit! Deserunt, dolore recusandae quisquam perferendis tempore vitae perspiciatis id provident ratione excepturi. Deleniti fugiat aspernatur eius eos at odit laudantium omnis ex voluptate error, velit cumque quibusdam ad. Nobis dicta itaque accusantium reprehenderit laborum dolores repudiandae. Soluta sed aliquid distinctio nihil dolorem. Quo non reprehenderit dignissimos, molestiae repellendus accusantium nobis nam autem atque nulla libero totam aliquid, minus nemo reiciendis porro dolores possimus eligendi quisquam? Dolores, et ut aspernatur non cum ipsum consectetur cupiditate magnam, fugit animi eveniet necessitatibus fuga ipsam, odio deleniti culpa ullam tempora esse voluptas beatae! Tenetur molestiae dicta dolorum assumenda, doloremque facilis, nemo quam beatae vel modi voluptates! Minus ratione in mollitia dolores enim nesciunt error qui molestias sed. Facilis nisi sunt, reprehenderit blanditiis, incidunt nesciunt corporis nobis eaque ut dolores natus, quia iusto repudiandae quis dolorem quaerat assumenda! Dicta, libero natus est vero numquam ipsam, enim iusto labore facere minima nobis omnis quaerat! Qui numquam error nihil molestias saepe corrupti aliquam ut tempora provident!</p>
     </div>
   )
 }
