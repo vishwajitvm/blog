4d52a116bab89c7a16151fcd8334be54cd660d27
vishwajitvm <vishwajitmall50@gmail.com>
2023-02-19 16:28:49 +0530

    useEffect with condition | part 2 | like component did mount

--- src/components/Hooks/MainHooks.js
+++ src/components/Hooks/MainHooks.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import UseEffectsHookSimpleComponent from './UseEffectsHooksComponts/UseEffectsHookSimpleComponent'
+import React, { useState } from 'react'
+
+// import UseEffectsHookSimpleComponent from './UseEffectsHooksComponts/UseEffectsHookSimpleComponent'
+import UseEffectsHooksWithConditionComponent from './UseEffectsHooksComponts/UseEffectsHooksWithConditionComponent'
 
 export default function MainHooks() {
+    const[count,SetCount] = useState(0) ;
+    const[data,SetData] = useState(0) ;
+
   return (
     <div>
-      <UseEffectsHookSimpleComponent />
+      {/* <UseEffectsHookSimpleComponent /> */}
+      <UseEffectsHooksWithConditionComponent count={count} data={data} />
+
+        <button onClick={()=>SetCount(count + 1)} >Update Count</button> &nbsp;
+        <button onClick={()=>SetData(data + 1)} >Update Data</button>
+
     </div>
   )
 }
